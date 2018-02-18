15c1b55bca9f5c2440941c303ec4d6d80f05b6ab
laden666666 <njz198869@gmail.com>
2018-02-19 00:06:44 +0800

    提交博客内容发布

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,7 @@ import {Link} from './Link'
 Vue.use(Vuex)
 
 declare function require<T>(name: string): T
-const linkList = require<Link[]>('../data/linkData1.json')
+const linkList = require<Link[]>('../data/linkData.json')
 
 export default new Vuex.Store<{
     selectPath: any[],
